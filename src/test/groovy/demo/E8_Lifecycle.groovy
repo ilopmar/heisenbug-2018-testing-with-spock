0fd88a9dbaaf7333477eacb9c9aa8290c9018e6a
Iván López <lopez.ivan@gmail.com>
2018-05-07 18:09:20 +0200

    Example 8: Spock Life cycle

---
+++ src/test/groovy/demo/E8_Lifecycle.groovy
@@ -0,0 +1,32 @@
+package demo
+
+import spock.lang.Specification
+
+class E8_Lifecycle extends Specification {
+
+    def setupSpec() {
+        println '> setupSpec'
+    }
+
+    def setup() {
+        println '>> setup'
+    }
+
+    def cleanup() {
+        println '>> cleanup'
+    }
+
+    def cleanupSpec() {
+        println '> cleanSpec'
+    }
+
+    void 'test'() {
+        expect:
+            println '>>> test'
+    }
+
+    void 'test 2'() {
+        expect:
+            println '>>> test 2'
+    }
+}
